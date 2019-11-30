b3b989fd05be9e07cf2077f2987eed75f6f327ef
Bruno Neves <brunu.neves@gmail.com>
2019-11-30 18:43:29 -0300

    Auth & create user

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace Modules\User\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Modules\User\Entities\User;
 
 class UserController extends Controller
 {
@@ -12,68 +13,33 @@ class UserController extends Controller
      * Display a listing of the resource.
      * @return Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('user::index');
+        return response()->json(User::all());
     }
 
     /**
      * Show the form for creating a new resource.
      * @return Response
      */
-    public function create()
-    {
-        return view('user::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'      => 'required|string',
+            'email'     => 'required|string|email|unique:users',
+            'password'  => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+        $user->save();
+
+        return response()->json([
+            'message' => 'Usuário cadastrado com sucesso'
+        ], 201);
     }
 
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        return view('user::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        return view('user::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -13,6 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
+    Route::get('/', 'UserController@index');
+    Route::post('/', 'UserController@store');
+});
+
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group(['middleware' => 'auth:api'], function() {
+
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'UserController@user');
+    });
 });
