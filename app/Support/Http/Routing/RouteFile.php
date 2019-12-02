8f4251f7a7e4a7c2d711a9c92b8239ed748d12aa
Bruno Neves <brunu.neves@gmail.com>
2019-12-01 22:33:57 -0300

    Support Factory and RouteFile

---
+++ app/Support/Http/Routing/RouteFile.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Support\Http\Routing;
+
+/**
+ * Class RouteFile.
+ */
+abstract class RouteFile
+{
+    /**
+     * @var array
+     */
+    protected $options;
+    /**
+     * @var \Illuminate\Routing\Router
+     */
+    protected $router;
+    /**
+     * RouteFile constructor.
+     * @param array $options
+     */
+    public function __construct($options = [])
+    {
+        $this->options = $options;
+        $this->router = app('router');
+    }
+    /**
+     *
+     */
+    public function register()
+    {
+        $this->router->group($this->options, function() {
+            $this->routes();
+        });
+    }
+    /**
+     * @return mixed
+     */
+    abstract protected function routes();
+}
