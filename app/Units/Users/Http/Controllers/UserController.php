db772b96f97a11c2c83d938515e343a7127cc946
Bruno Neves <brunu.neves@gmail.com>
2019-12-10 22:45:32 -0300

    methods users

--- app/Units/Users/Http/Controllers/UserController.php
+++ app/Units/Users/Http/Controllers/UserController.php
@@ -2,31 +2,66 @@
 
 namespace App\Units\Users\Http\Controllers;
 
+use App\Domains\Users\Repositories\UserRepository;
 use App\Support\Http\Controllers\Controller;
+use App\Units\Users\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
 
-    public function __construct()
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
     {
         $this->middleware('auth');
+        $this->userRepository = $userRepository;
     }
 
     public function index()
     {
+        try {
+            $users = $this->userRepository->getAll();
+            return response()->json($users, Response::HTTP_OK);
 
+        } catch (\Exception $e) {
+            return  response()->json([
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function show($id)
     {
+        try {
+            $user = $this->userRepository->findId($id);
+            return response()->json($user, Response::HTTP_OK);
 
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
+        try {
+            $data = $request->all();
+            $user = $this->userRepository->create($data);
 
+            if ($user) {
+                return response()->json($user, Response::HTTP_OK);
+            }
+
+            return response()->json('Ocorreu um error interno', Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     public function update($id, Request $request)
