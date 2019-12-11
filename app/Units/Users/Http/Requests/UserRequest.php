d4d0fc6a48c36bceda3ec515391ae134bc567ec2
Bruno Neves <brunu.neves@gmail.com>
2019-12-10 22:46:04 -0300

    Rules userRequest

---
+++ app/Units/Users/Http/Requests/UserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Units\Users\Http\Requests;
+
+use App\Support\Http\Request;
+
+class UserRequest extends Request
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+        ];
+    }
+}
