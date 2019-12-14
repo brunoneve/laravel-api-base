ba22967eccc6ce5c56ae59164a0681ee93b5b367
Bruno Neves <brunu.neves@gmail.com>
2019-12-14 01:34:39 -0300

    create & update role and permissions associated

--- app/Units/ACL/Http/Controllers/RoleController.php
+++ app/Units/ACL/Http/Controllers/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Units\ACL\Http\Controllers;
 
 use App\Support\Http\Controllers\Controller;
 use App\Domains\ACL\Repositories\RoleRepository;
+use App\Domains\ACL\Repositories\PermissionRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -11,11 +12,13 @@ use Illuminate\Http\Response;
 class RoleController extends Controller
 {
     protected $roleRepository;
+    protected $permissionRepository;
 
-    public function __construct(RoleRepository $roleRepository)
+    public function __construct(RoleRepository $roleRepository, PermissionRepository $permissionRepository)
     {
         $this->middleware('auth');
         $this->roleRepository = $roleRepository;
+        $this->permissionRepository = $permissionRepository;
     }
 
     public function index()
@@ -54,8 +57,19 @@ class RoleController extends Controller
     public function store(Request $request)
     {
         try {
-            $data = $request->all();
+            $this->validate($request, [
+                'name'=>'required|unique:roles|max:20',
+                'permissions' =>'required',
+            ]);
+
+            $data = $request->except(['permissions']);
+            $permissions = $request['permissions'];
             $role = $this->roleRepository->create($data);
+
+            foreach ($permissions as $permission) {
+                $p = $this->permissionRepository->findId($permission);
+                $role->givePermissionTo($p);
+            }
 
             if ($role) {
                 return response()->json($role, Response::HTTP_OK);
@@ -73,15 +87,31 @@ class RoleController extends Controller
     public function update($id, Request $request)
     {
         try {
-
             $role = $this->roleRepository->findId($id);
 
             if (!$role) {
                 return response()->json(['error' => 'Registro não encontrado!'], Response::HTTP_NOT_FOUND);
             }
 
-            $data = $request->all();
+            $this->validate($request, [
+                'name' => 'required|max:20|unique:roles,name,'.$id,
+                'permissions' => 'required',
+            ]);
+
+            $data = $request->except(['permissions']);
+            $perms = $request['permissions'];
             $this->roleRepository->update($role, $data);
+
+            $permissions = $this->permissionRepository->getAll(0,false); //All permissions
+
+            foreach ($permissions as $p) {
+                $role->revokePermissionTo($p); //Remove all permissions associated with role
+            }
+
+            foreach ($perms as $perm) {
+                $p = $this->permissionRepository->findId($perm); //Get corresponding form //permission in db
+                $role->givePermissionTo($p);  //Assign permission to role
+            }
 
             return response()->json($role, Response::HTTP_OK);
 
